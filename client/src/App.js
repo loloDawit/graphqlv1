9d30a068aecfd63c52fbaf9d47b6b7224936cb3e
Dawit Abera <dawit.y.abera@nordstrom.com>
2019-12-26 01:23:42 -0800

    Added apollo lib to use graphql server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks'
+import Launches from './components/Launches'
 import './App.css';
 import logo from './logo.png'
+
+const client = new ApolloClient({
+  uri:'http://localhost:5000/graphql'
+});
+
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
+    <div className="container">
       <img src={logo} alt="SpaceX" style={{width:300, display:'block', margin:'auto'}} />
+      <Launches />
     </div>
+    </ApolloProvider>
   );
 }
 
