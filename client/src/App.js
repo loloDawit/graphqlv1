af0aa66a4683a59ad23e1ebbb98942a684970fb1
Dawit Abera <dawit.y.abera@nordstrom.com>
2019-12-26 08:28:57 -0800

    Added launch component and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+import Launch from './components/Launch'
 import Launches from './components/Launches'
 import './App.css';
 import logo from './logo.png'
@@ -12,11 +14,15 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+    <Router>
     <div className="container">
       <img src={logo} alt="SpaceX" style={{width:300, display:'block', margin:'auto'}} />
-      <Launches />
+      <Route exact path="/" component={Launches} />
+      <Route exact path="/launch/:flight_number" component={Launch} />
     </div>
+    </Router>
     </ApolloProvider>
+
   );
 }
 
